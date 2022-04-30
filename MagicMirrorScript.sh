4c8df8827a62d99a75998878665e061ccb05ecd0
Jeevith Hegde <jeevith20@gmail.com>
2022-04-30 15:57:05 +0200

    pushing first commit

---
+++ MagicMirrorScript.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Installing nodejs and npm
+sudo apt update -y 
+sudo apt install nodejs -y && sudo apt install npm -y
+
+# Installing MagicMirror 
+cd "$(dirname "$0")" && curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+git clone https://github.com/MichMich/MagicMirror
+
+echo "CD and npm install"
+cd "$(dirname "$0")/MagicMirror" && npm install --only=prod --omit=dev && npm audit fix  --force
+npm install postman-request
+
+
+echo "Copy config sample"
+cp "config/config.js.sample" "config/config.js" 
+
+cd "$(dirname "$0")/modules" && git clone https://github.com/YR/MMM-YrNow
+# The author has not fixed two errors (request module depricated and incorrect url) Lets fix it
+sed  -i 's/request/postman-request/2'  "$(dirname "$0")/MMM-YrNow/node_helper.js"
+sed -i 's/\/id//1' "$(dirname "$0")/MMM-YrNow/MMM-YrNow.js"
+
+# Install MMM-Worldclock 
+git clone https://github.com/BKeyport/MMM-Worldclock 
+
+
+# Install MMM-PostDelivery-Norway
+git clone https://github.com/reidarw/MMM-PostDelivery-Norway.git
+# The author has not fixed one error (request module depricated)
+sed  -i 's/request/postman-request/2'  "$(dirname "$0")/MMM-PostDelivery-Norway/node_helper.js"
+
+# Install MMM-TRV-WastePlan
+git clone https://github.com/reidarw/MMM-TRV-WastePlan.git
+# The author has not fixed one error (request module depricated)
+sed  -i 's/request/postman-request/2'  "$(dirname "$0")/MMM-TRV-WastePlan/node_helper.js"
+
+# Install MMM-Entur-tavle
+git clone https://github.com/Arve/MMM-Entur-tavle.git
+
+# Install MMM-Tibber
+git clone https://github.com/ottopaulsen/MMM-Tibber
+cd "$(dirname "$0")/MMM-Tibber" && npm install && npm audit fix --force
+
+# Back to modules install MMM-Tools
+cd .. 
+git clone https://github.com/bugsounet/MMM-Tools
+cd "$(dirname "$0")/MMM-Tools" && npm install && npm audit fix --force
+
+# Back to modules install
+cd .. 
+
+
+echo "start MagicMirror"
+cd ..
+npm run start
+
+cd ..
+
+# Copy the config file
+cp /home/$USER/Documents/config.js   /home/$USER/Documents/MagicMirror/config/config.js 
+
+
---
+++ config.js
@@ -0,0 +1,230 @@
+/* MagicMirror² Config Sample
+ *
+ * By Michael Teeuw https://michaelteeuw.nl
+ * MIT Licensed.
+ *
+ * For more information on how you can configure this file
+ * see https://docs.magicmirror.builders/configuration/introduction.html
+ * and https://docs.magicmirror.builders/modules/configuration.html
+ */
+let config = {
+	address: "0.0.0.0", 	// Address to listen on, can be:
+	// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
+	// - another specific IPv4/6 to listen on a specific interface
+	// - "0.0.0.0", "::" to listen on any interface
+	// Default, when address config is left out or empty, is "localhost"
+	port: 8086,
+	basePath: "/", 	// The URL path where MagicMirror² is hosted. If you are using a Reverse proxy
+	// you must set the sub path here. basePath must end with a /
+	ipWhitelist: [], 	// Set [] to allow all IP addresses
+	// or add a specific IPv4 of 192.168.1.5 :
+	// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
+	// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
+	// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
+
+	useHttps: false, 		// Support HTTPS or not, default "false" will use HTTP
+	httpsPrivateKey: "", 	// HTTPS private key path, only require when useHttps is true
+	httpsCertificate: "", 	// HTTPS Certificate path, only require when useHttps is true
+
+	language: "en",
+	locale: "en-US",
+	logLevel: ["INFO", "LOG", "WARN", "ERROR"], // Add "DEBUG" for even more logging
+	timeFormat: 24,
+	units: "metric",
+	// serverOnly:  true/false/"local" ,
+	// local for armv6l processors, default
+	//   starts serveronly and then starts chrome browser
+	// false, default for all NON-armv6l devices
+	// true, force serveronly mode, because you want to.. no UI on this device
+
+	modules: [
+		{
+			module: "alert",
+		},
+		{
+			module: "updatenotification",
+			position: "top_bar"
+		},
+		{
+			module: "clock",
+			position: "top_bar",
+		},
+
+		{
+			module: "MMM-Entur-tavle",
+			position: "bottom_right",
+			header: "AtB - Now +5 minutes",
+			config: {
+				stopId: '42632',
+				stopType: 'StopPlace',
+				numResults: 5,
+				showName: false,
+				highlightRealtime: true,
+				showTransportMode: true,
+				refresh: 120,
+				size: "small",
+				delay: [5, 'minutes'],
+
+
+			}
+		},
+
+		{
+			module: "MMM-Entur-tavle",
+			position: "bottom_right",
+			config: {
+				stopId: '43670',
+				stopType: 'StopPlace',
+				numResults: 5,
+				showName: false,
+				highlightRealtime: true,
+				showTransportMode: true,
+				refresh: 120,
+				size: "small",
+				delay: [5, 'minutes'],
+
+
+			}
+		},
+
+
+		{
+			module: "newsfeed",
+			position: "bottom_bar",
+			config: {
+				feeds: [
+					{
+						title: "NRK",
+						url: "https://www.nrk.no/nyheter/siste.rss"
+					}
+				],
+				showSourceTitle: true,
+				showPublishDate: true,
+				broadcastNewsFeeds: true,
+				broadcastNewsUpdates: true
+			}
+		},
+ 		{
+			module: "newsfeed",
+			position: "bottom_bar",
+			config: {
+				feeds: [
+					{
+						title: "e24",
+						url: "https://e24.no/rss2/?seksjon=boers-og-finans"
+					}
+				],
+				showSourceTitle: true,
+				showPublishDate: true,
+				broadcastNewsFeeds: true,
+				broadcastNewsUpdates: true
+			}
+		},
+		{
+			module: "MMM-Tibber",
+			header: "Tibber",
+			position: "top_right",
+			config: {
+				tibberToken: "",
+				houseNumber: 2,
+				showPowerGauge: false,
+				showVoltageGauge: false,
+				showCurrentGauge: false,
+				graphWidth: 600,
+
+			}
+		},
+
+		{
+			module: 'MMM-YrNow',
+			position: 'top_left',
+			config: {
+				locationId: '10-1023731',
+				showWeatherForecast: true
+			}
+		},
+
+			{
+			module: 'MMM-PostDelivery-Norway',
+			position: 'top_left',
+			config: {
+				zipCode: 7083
+			}
+		},
+
+
+		{
+			module: "MMM-TRV-WastePlan",
+			header: "Tømmeplan",
+			position: "bottom_left",
+			config: {
+				id: 2767,
+				header: "",
+				blnLabel: true,
+				blnDate: true,
+			}
+		},
+
+
+
+
+
+		{
+			module: 'MMM-Tools',
+			position: 'bottom_left',
+			config: {
+				refresh: 1000 * 5,
+				itemSIze: null,
+				conntainerSize: null,
+				MM: { displayMM: false, },
+				NODE: { displayNode: false, },
+				NPM: { displayNpm: false, },
+				CPU: { orderUsage: 3, displayGovernor: false, displayType: false, },
+				UPTIME: { displayUptime: false, displayRecord: false, },
+				STORAGE: { partitionExclude: ["/boot", "/"], },
+				NETWORK: { orderNetwork: 1, },
+			}
+		},
+
+
+		{
+			module: 'MMM-Worldclock',
+			position: 'top_center', // This can be any of the regions, best in top_left or top_right regions
+			config: {
+				// See 'Configuration options' for more information.
+				timeFormat: 'hh:mm A', //Global time format, as defined in moment.js format()
+				style: 'left', // Which way do you want the flag and description from the clock? choices are 'top', 'left','right','bottom'
+				offsetTimezone: null, // Timezone you want to show the difference from. null, "", or omitted from config will be UTC.
+				clocks: [
+					{
+						title: "Home",
+						flag: "no"
+					},
+					{
+						title: "Udupi", // Too long of a title could cause bad text align.
+						timezone: "Asia/Kolkata", //When omitted, Local time will be displayed.
+						flag: "in", // If you'd like a flag from the standard library
+					},
+					{
+						timezone: "Asia/Dubai",
+						flag: "ae"
+					},
+					{
+						title: "UTC",
+						timezone: "UTC",
+						timeFormat: "HH:mm DD/MM", // Time format override.
+						altflag: "world.png" // if you'd like a flag from a file on your mirror device.
+					},
+				]
+			},
+		},
+
+
+
+
+
+	]
+};
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== "undefined") { module.exports = config; }
